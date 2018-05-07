ee418d5933e6ccdf91741a9ae0b994e5c24422a3
Tomas <tomis.jasulaitis@gmail.com>
2018-05-07 13:34:20 -0700

    Updated profile interface

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\Type\MemberType;
 use App\Entity\UserInfo;
+use App\Entity\Member;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,13 +20,15 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class ProfileController extends Controller
 {
     /**
-     * @Route("/profile", name="profile")
+     * @Route("/profile/profileCreate/{id}", name="profileCreate")
      */
-    public function ChangeProfileAction(Request $request)
+    public function CreateProfileAction(Request $request)
     {
-        $UserInfo = new UserInfo();
 
-        $form = $this->createFormBuilder($UserInfo)
+        $userInfo = new UserInfo();
+        $member = $this->getUser();
+
+        $form = $this->createFormBuilder($userInfo)
 
          ->add('firstName', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
          ->add('lastName', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
@@ -34,19 +37,129 @@ class ProfileController extends Controller
             ->add('city', TextAreaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
             ->add('address', TextAreaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
             ->add('description', TextAreaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
-
-        ->add('submit', SubmitType::class, array('label' => 'Update'))
-         ->getForm();
+        ->add('submit', SubmitType::class, array('label' => 'Create'))
+            ->getForm();
 
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid())
         {
-            die('submitted');
+            $firstName = $form['firstName']->getData();
+            $lastName = $form['lastName']->getData();
+            $bornAt = $form['bornAt']->getData();
+            $country = $form['country']->getData();
+            $city = $form['city']->getData();
+            $address = $form['address']->getData();
+            $description = $form['description']->getData();
+
+            $userInfo->setFirstName($firstName);
+            $userInfo->setLastName($lastName);
+            $userInfo->setBornAt($bornAt);
+            $userInfo->setCountry($country);
+            $userInfo->setCity($city);
+            $userInfo->setAddress($address);
+            $userInfo->setDescription($description);
+            $userInfo ->setMember($member);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($userInfo);
+            $em->persist($member);
+            $em->flush();
+
+            return $this->redirectToRoute('profileDetails',   array(
+                'id' => $userInfo->getId()));
         }
 
-        return $this->render('profile/profile.html.twig',
-            array('form' => $form->createView()));
+        return $this->render('profile/profileCreate.html.twig',
+            array(
+                'userInfo' => $userInfo,
+                'form' => $form->createView()));
+
+    }
+
+    /**
+     * @Route("/profile/profileDetails/{id}", name="profileDetails")
+     */
+    public function ShowProfileAction($id, Request $request)
+    {
+
+        $userInfo = $this->getDoctrine()->getRepository('App:UserInfo')
+            ->find($id);
+
+
+        return $this->render('profile/profileDetails.html.twig',
+            array('userInfo' => $userInfo));
+
+    }
+
+    /**
+     * @Route("/profile/profileEdit/{id}", name="profileEdit")
+     */
+    public function ProfileEditAction($id, Request $request)
+    {
+        $userInfo = $this->getDoctrine()->getRepository('App:UserInfo')
+            ->find($id);
+
+
+        $userInfo->setFirstName($userInfo->getFirstName());
+        $userInfo->setLastName($userInfo->getLastName());
+        $userInfo->setBornAt($userInfo->getBornAt());
+        $userInfo->setCountry($userInfo->getCountry());
+        $userInfo->setCity($userInfo->getCity());
+        $userInfo->setAddress($userInfo->getAddress());
+        $userInfo->setDescription($userInfo->getDescription());
+
+
+        $form = $this->createFormBuilder($userInfo)
+            ->add('firstName', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
+            ->add('lastName', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
+            ->add('bornAt', DateTimeType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
+            ->add('country', TextAreaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
+            ->add('city', TextAreaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
+            ->add('address', TextAreaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
+            ->add('description', TextAreaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin bottom:15px')))
+            ->add('submit', SubmitType::class, array('label' => 'Update'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $firstName = $form['firstName']->getData();
+            $lastName = $form['lastName']->getData();
+            $bornAt = $form['bornAt']->getData();
+            $country = $form['country']->getData();
+            $city = $form['city']->getData();
+            $address = $form['address']->getData();
+            $description = $form['description']->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $userInfo = $em->getRepository('App:UserInfo')
+                ->find($id);
+
+            $userInfo->setFirstName($firstName);
+            $userInfo->setLastName($lastName);
+            $userInfo->setBornAt($bornAt);
+            $userInfo->setCountry($country);
+            $userInfo->setCity($city);
+            $userInfo->setAddress($address);
+            $userInfo->setDescription($description);
+
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Profile Updated'
+            );
+            return $this->redirectToRoute('profileDetails',   array(
+                'id' => $userInfo->getId()));
+        }
+
+        return $this->render('profile/profileEdit.html.twig',
+            array(
+                'userInfo' => $userInfo,
+                'form' => $form->createView()));
+
 
 
     }
---
+++ src/Migrations/Version20180507193026.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180507193026 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_info DROP FOREIGN KEY FK_B1087D9E7597D3FE');
+        $this->addSql('ALTER TABLE user_info ADD CONSTRAINT FK_B1087D9E7597D3FE FOREIGN KEY (member_id) REFERENCES member (id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user_info DROP FOREIGN KEY FK_B1087D9E7597D3FE');
+        $this->addSql('ALTER TABLE user_info ADD CONSTRAINT FK_B1087D9E7597D3FE FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE');
+    }
+}
