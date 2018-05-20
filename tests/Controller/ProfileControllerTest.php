ff995f78c0e0bc8861cb3aff97f5e77879683cc5
Tomas <tomis.jasulaitis@gmail.com>
2018-05-20 22:17:20 +0300

    Code tweaks

--- tests/Controller/ProfileControllerTest.php
+++ tests/Controller/ProfileControllerTest.php
@@ -16,14 +16,12 @@ class ProfileControllerTest extends WebTestCase
 {
     private $client = null;
 
-    public function setUp()
-    {
+    public function setUp(){
         $this->client = static::createClient();
         $this->crawler = $this->client->request('GET', '/');
     }
 
-    public function testShowHomepage()
-    {
+    public function testShowHomepage(){
         $client = static::createClient();
 
         $client->request('GET', '/');
@@ -31,26 +29,22 @@ class ProfileControllerTest extends WebTestCase
     }
 
 
-    public function testShowAboutUs()
-    {
+    public function testShowAboutUs(){
         $crawler = $this->client->request('GET', '/aboutus');
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testShowPrices()
-    {
+    public function testShowPrices(){
         $crawler = $this->client->request('GET', '/prices');
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testShowServices()
-    {
+    public function testShowServices(){
         $crawler = $this->client->request('GET', '/services');
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testResetPasswordSendsEmail()
-    {
+    public function testResetPasswordSendsEmail(){
         $client = static::createClient();
         $crawler = $this->client->request('GET', '/resetPassword');
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
@@ -61,38 +55,31 @@ class ProfileControllerTest extends WebTestCase
     }
 
 
-    public function testContactSendsEmail()
-    {
+    public function testContactSendsEmail(){
         $client = static::createClient();
         $crawler = $this->client->request('GET', '/contact');
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
 
         $form = $crawler->selectButton('Submit')->form();
         $crawler = $client->submit($form, array('contact_form[name]' => 'Fabien', 'contact_form[email]' => 'tomas@gmail.com',
-        'contact_form[subject]' => 'anypass', 'contact_form[message]' => 'hello'    ));
+            'contact_form[subject]' => 'anypass', 'contact_form[message]' => 'hello'));
 
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 
-
-
-
-
-    public function testRegisterUser()
-    {
+    public function testRegisterUser(){
         $client = static::createClient();
         $crawler = $this->client->request('GET', '/register');
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
 
         $form = $crawler->selectButton('Register')->form();
         $crawler = $client->submit($form, array('appbundle_member[username]' => 'Fabien', 'appbundle_member[email]' => 'tomas@gmail.com',
-            'appbundle_member[plainPassword][first]' => 'anypass' ,  'appbundle_member[plainPassword][second]' => 'anypass'    ));
+            'appbundle_member[plainPassword][first]' => 'anypass', 'appbundle_member[plainPassword][second]' => 'anypass'));
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
 
     }
 
-    public function testCreateProfile()
-    {
+    public function testCreateProfile(){
         $client = static::createClient();
 
         $crawler = $this->client->request('GET', '/login');
@@ -100,48 +87,11 @@ class ProfileControllerTest extends WebTestCase
 
         $form = $crawler->selectButton('Sign in')->form();
         $crawler = $client->submit($form, array('_username' => 'Fabien',
-            '_password' => 'anypass'    ));
+            '_password' => 'anypass'));
 
-        //$crawler = $this->client->followRedirect(true);
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-
-       // $crawler = $this->client->request('GET', '/profile/create');
-     //   $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-
     }
-
-
-   /* public function testShowProfile()
-    {
-        $this->doLogin();
-        $crawler = $this->client->request('GET', '/profile/details/1');
-        $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-    }*/
-
-
-
-      /*  public function testRegister()
-        {
-           // $crawler = $this->client->request('GET', '/register');
-         //   $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-         //   $form = $crawler->selectButton('Register')->form();
-           // var_dump($form);
-       //     $form['username'] = 'symfonyfan';
-           // var_dump($crawler);
-           // var_dump($form);
-         //   $form['username'] = 'symfonyfan';
-          //  $form['email'] = 'symfonyfan@gmail.com';
-          //  $form['first'] = 'anypass';
-         //   $form['second'] = 'anypass';
-         /*   $crawler = $this->client->submit($form);
-            $crawler = $this->client->followRedirect(true);
-            $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());*/
-
 }
-
-
-
-
 
 
 ?>
